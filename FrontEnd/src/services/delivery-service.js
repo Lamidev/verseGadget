ac2b1206e91fb039bade408524bbc6e774399fcc
thisslami <akinyemioluwaseunjunior@gmail.com>
2025-09-05 15:17:40 +0100

    made changes

--- FrontEnd/src/services/delivery-service.js
+++ FrontEnd/src/services/delivery-service.js
@@ -1,11 +1,11 @@
 // deliveryService.js
 export const getDeliveryPrice = (state) => {
     const deliveryPrices = {
-      'Lagos': 5000,
-      'Abuja': 15000,
-      'Port Harcourt': 12000,
-      "Ibadan": 10000,
+      'Lagos': 3000,
+      'Abuja': 10000,
+      'Port Harcourt': 10000,
+      "Ibadan": 5000,
       // Add more cities and prices as needed
     };
-    return deliveryPrices[state] || 20000; // Default price if city not found
+    return deliveryPrices[state] || 15000; // Default price if city not found
   };
